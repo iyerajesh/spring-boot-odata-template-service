1259a6690ece9bd2f9bbdbef599da5a0153885c4
Rajesh Iyer <rajeshiyer@Rajeshs-MacBook-Pro.local>
2016-08-10 14:21:50 -0400

    Initial commit

---
+++ src/main/java/com/axa/spring/odata/exception/EdmException.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.axa.spring.odata.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * @author Rajesh Iyer
+ *
+ */
+@ResponseStatus(value=HttpStatus.INTERNAL_SERVER_ERROR,reason = "Error Processing Edm")
+public class EdmException extends RuntimeException {
+
+}
